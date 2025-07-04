6f07b5cdc447cb0f30483e8093acafd61d0badaf
Arsh Ansari <arsh.nitw@gmail.com>
2025-07-05 00:13:08 +0530

    Update assets.js

--- src/testinger_frontend/src/assets/assets.js
+++ src/testinger_frontend/src/assets/assets.js
@@ -1,7 +1,9 @@
 import hero from './Hero.png';
+import introVideo from './intro.mp4'; // rename to reflect it's a video
 
 const assets = {
-  hero
+  hero,
+  video: introVideo
 };
 
 export default assets;
